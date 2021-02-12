7b69a58dbae068d45642575d39aece6da32b9dde
Christian Fernandez <christian.fernandez@ensicaen.fr>
2021-02-12 23:59:17 +0100

    WIP

--- conda/build.sh
+++ conda/build.sh
@@ -2,9 +2,9 @@
 
 # setup
 
-SCP_DATA_DIR=$HOME/.spectrochempy/spectrochempy_data/
+SCP_DATA_DIR=$HOME/.spectrochempy
 mkdir -p $SCP_DATA_DIR
 
-DATA_DIR=$SCP_DATA_DIR/
+DATA_DIR=$SCP_DATA_DIR//spectrochempy_data/
 mkdir -p $DATA_DIR
 cp -a $RECIPE_DIR/../testdata/ $DATA_DIR/
