134a82271cfb20355b70cfb730a5292763b1ab42
Christian Fernandez <christian.fernandez@ensicaen.fr>
2021-02-13 18:04:20 +0100

    wip

--- recipe/build.bat
+++ recipe/build.bat
@@ -3,5 +3,5 @@ IF NOT EXIST "%DATA_DIR%" MKDIR "%DATA_DIR%"
 
 XCOPY "%RECIPE_DIR%\..\testdata" "%DATA_DIR%"   /Y /S
 
-REM post-script being not executed on windows this is a try for a workaround
+REM post-link script being not executed on windows this is a try for a workaround
 XCOPY "%RECIPE_DIR%\post_link.bat" "%PREFIX%\Script\.spectrochempy_data-post-link.bat"
