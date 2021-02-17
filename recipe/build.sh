ffe1aefb3497f24db9dbd8a472fe6fdf97b69a0d
Christian Fernandez <christian.fernandez@ensicaen.fr>
2021-02-17 17:24:35 +0100

    Cleaning

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,4 @@
 DATA_DIR=$PREFIX/share/spectrochempy_data/
 mkdir -p $DATA_DIR
 
-cp -a $RECIPE_DIR/../testdata/ $DATA_DIR/
+cp -a $RECIPE_DIR/../testdata/ $DATA_DIR
--- recipe/post-link.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DATA_DIR=$HOME/.spectrochempy/
-SRC_DIR=$CONDA_PREFIX/share/spectrochempy_data/
-
-mv -f SRC_DIR/testdata/ $DATA_DIR/
